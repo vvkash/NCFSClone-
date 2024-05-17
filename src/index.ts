3972852005b858d1da9e5c9165c6b857d78c1425
Aakash Shah <121144051+vvkash@users.noreply.github.com>
2024-05-17 14:05:13 -0400

    updated typescript code too upload videos

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,32 @@ import ffmpeg from "fluent-ffmpeg"
 
 
 const app = express();
-const port = 3000;
 
 app.get ("/", (req, res) => {
-    res.send("Hello World!");
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath)  {
+        res.status(400).send("Bad Request: Missing file path.");
+
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions('-vf', "scale=-1:360") //360p
+        .on("end", () => {
+             res.status(200).send("Video Processing has started!")
+
+        })
+        .on("error", (err) => {
+           console.log(`error occured: ${err.message}`)
+           res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(outputFilePath);
 });
+
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
     console.log(
